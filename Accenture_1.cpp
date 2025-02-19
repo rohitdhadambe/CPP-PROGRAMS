e747b8685b84ca961c3a1f5fd02c8ec2fd7204c6
rohitdhadambe <rohitdhadambe@gmail.com>
2025-02-19 19:16:44 +0530

    Accenture problem

---
+++ Accenture_1.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[] = { 3, 3, 4, 4};
+    int count = 0;
+    int n = 4; // Number of elements in the array
+
+    for (int i = 0; i < n - 1; i++) {
+        if (arr[i] == 3 && arr[i + 1] == 4) {
+            swap(arr[i], arr[i + 1]);
+            count++;
+            i=-1;
+        }
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout<<"\n";
+    }
+
+    // Printing the updated array
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    
+    cout << "\n" << count;
+    return 0;
+}
