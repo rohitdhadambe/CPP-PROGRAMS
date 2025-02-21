e0e380ab49c698928ce19d27c4f94f1e2087497d
rohitdhadambe <rohitdhadambe@gmail.com>
2025-02-21 11:03:54 +0530

    .

--- DiamondPattern.cpp
+++ DiamondPattern.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 using namespace std;
-int main() {  
-    
-    
 
+int main() {
+    int n = 11; // Always odd
 
-return 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            // Print '*' at borders, diagonals, and specific conditions
+            if (i == 0 || i == n - 1 || j == 0 || j == n - 1 || i == j || i + j == n - 1 || i == n / 2 || j == n / 2) {
+                cout << "*";
+            } else {
+                cout << " ";
+            }
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
