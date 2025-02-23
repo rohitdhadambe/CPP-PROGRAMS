0d1dfbb7fee345e529af72140abc5d67ecd63d48
rohitdhadambe <rohitdhadambe@gmail.com>
2025-02-23 14:41:15 +0530

    .

--- Facebook.cpp
+++ Facebook.cpp
@@ -1,7 +1,38 @@
 #include <iostream>
+#include <string>
+#include <stack>
 using namespace std;
-int main() {
 
-    
-return 0;
+string decodeString(string s) {
+    stack<int> numStack; // Stack for numbers
+    stack<string> strStack; // Stack for substrings
+    string currStr = "";
+    int num = 0;
+
+    for (char c : s) {
+        if (isdigit(c)) {
+            num = c - '0'; // Handle multi-digit numbers
+        } else if (c == '[') {
+            numStack.push(num);
+            strStack.push(currStr);
+            num = 0;
+            currStr = "";
+        } else if (c == ']') {
+            int repeatCount = numStack.top(); numStack.pop();
+            string temp = currStr;
+            currStr = strStack.top(); strStack.pop();
+            while (repeatCount--) {
+                currStr += temp;
+            }
+        } else {
+            currStr += c;
+        }
+    }
+    return currStr;
+}
+
+int main() {
+    string s = "3[a2[c]]";
+    cout << decodeString(s) << endl; // Output: "aaabcbc"
+    return 0;
 }
