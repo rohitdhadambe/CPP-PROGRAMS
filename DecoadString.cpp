a86b447aecae9b34c215a71d6964b7dfc2d7ec3f
rohitdhadambe <rohitdhadambe@gmail.com>
2025-02-23 20:30:58 +0530

    decoadstring

--- DecoadString.cpp
+++ DecoadString.cpp
@@ -6,34 +6,40 @@ using namespace std;
 string decodeString(string s) {
     
     stack<int> numStack; // Stack for numbers
+
     stack<string> strStack; // Stack for substrings
+
     string currStr = "";
     int num = 0;
 
     for (char c : s) {
         if (isdigit(c)) {
             num = num * 10 + (c - '0'); // Handle multi-digit numbers
-        } else if (c == '[') {
+        } 
+        else if (c == '[') {
             numStack.push(num);
             strStack.push(currStr);
             num = 0;
             currStr = "";
-        } else if (c == ']') {
+        }
+         else if (c == ']') {
             int repeatCount = numStack.top(); numStack.pop();
             string temp = currStr;
             currStr = strStack.top(); strStack.pop();
             while (repeatCount--) {
                 currStr += temp;
             }
-        } else {
+        } 
+        else {
             currStr += c;
         }
     }
+    
     return currStr;
 }
 
 int main() {
     string s = "3[a2[bc]]";
-    cout << decodeString(s) << endl; // Output: "aaabcbc"
+    cout << decodeString(s) << endl; 
     return 0;
 }
