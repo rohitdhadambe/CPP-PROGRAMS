dbc5d71a525c77e64d09f6ad74884be08cdf7478
rohitdhadambe <rohitdhadambe@gmail.com>
2025-02-24 15:33:14 +0530

    Decennding Triangle

--- DecendingTriangle.cpp
+++ DecendingTriangle.cpp
@@ -1,28 +1,30 @@
-//  #include <iostream>
-//  using namespace std;
-//  int main() {
-//      for(int i=1; i<=4; i++){
-//         for(int j=i; j<i+i; j++){
-//             cout<<j;
-//         }
-//         cout<<"\n";
-//      } 
-//  return 0;
-//  }
-
-#include <iostream>
-using namespace std;
-
-int main() {
-    int rows = 4; // Number of rows in the pattern
-    for (int i = 1; i <= rows; i++) {
-        int num = i; // Starting number for each row
-        for (int j = 1; j <= i; j++) {
-            cout << num++;
+ #include <iostream>
+ using namespace std;
+ int main() {
+    int m=1;
+     for(int i=1; i<=4; i++){
+        for(int j=i; j<i+m; j++){
+            cout<<j;
         }
-        cout << endl;
-    }
-    return 0;
-}
+        m++;
+        cout<<"\n";
+     } 
+ return 0;
+ }
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int rows = 4; // Number of rows in the pattern
+//     for (int i = 1; i <= rows; i++) {
+//         int num = i; // Starting number for each row
+//         for (int j = 1; j <= i; j++) {
+//             cout << num++;
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
 
 
