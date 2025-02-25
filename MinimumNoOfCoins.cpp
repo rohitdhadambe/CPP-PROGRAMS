ed52ec7ad830d367b3b368a210d0d0bcdd5cf465
rohitdhadambe <rohitdhadambe@gmail.com>
2025-02-26 00:24:46 +0530

    Minimum NO Of coins

---
+++ MinimumNoOfCoins.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+  
+    int Target = 23 , i=0 , sum=0 , count=0;
+    vector <int> v1 = {5,2,1};
+
+    while(1){
+    if(sum==Target) {
+        cout<<count;
+        return 0;
+    }
+
+     if(sum<Target){
+        count++;
+       sum += v1[i];
+    }
+    if(sum>Target){
+        count--;
+       sum -= v1[i];
+       i++;
+    }
+}
+
+return 0;
+}
