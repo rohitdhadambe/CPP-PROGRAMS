deb647edbcc08774a61d84e4a7befa416bb92848
rohitdhadambe <rohitdhadambe@gmail.com>
2025-02-26 00:27:41 +0530

    .

--- MinimumNoOfCoins.cpp
+++ MinimumNoOfCoins.cpp
@@ -25,3 +25,34 @@ int main() {
 
 return 0;
 }
+
+
+
+
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//     int Target = 16, i = 0, sum = 0, count = 0;
+//     vector<int> v1 = {5, 2, 1};
+
+//     while (i < v1.size()) {
+//         if (sum == Target) {
+//             cout << count;
+//             return 0;
+//         }
+        
+//         if (sum < Target) {
+//             sum += v1[i];
+//             count++;
+//         } else {  // sum > Target
+//             sum -= v1[i];
+//             count--;
+//             i++;  // Move to the next element
+//         }
+//     }
+
+//     cout << "Not possible to reach target" << endl;
+//     return 0;
+// }
