9b5a26c54dd9acd484f1009d631801d108bbb7e4
rohitdhadambe <rohitdhadambe@gmail.com>
2025-03-02 15:11:52 +0530

    .

--- MinimumCostOfRope.cpp
+++ MinimumCostOfRope.cpp
@@ -3,33 +3,24 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-int main() {
-    vector <int>v1 ={1,2,3};
-    int n = 3 , sum = 0 , res = 0;
+int main() { 
+    vector <int>v1 ={4,2,3,6};
+    int n = 4 , sum = 0 , res = 0;
     priority_queue <int,vector<int>,greater<int>> q1; //min heap
     
     for (auto i : v1) q1.push(i);
 
-// while (!q1.empty())
-// {
-//     cout<<q1.top();
-//     q1.pop();
-// }
-
-
     int sum1=0, sum2=0;
     int i=0;
- while(i!=n-1){
 
+ while(i!=n-1){            // can use q1.size() > 1
      sum1 = q1.top();
      q1.pop();
      sum2 = q1.top();
      q1.pop();
     sum = sum1 + sum2;
     res = res + sum;
-    
      q1.push(sum);
-     cout<<"\n"<<sum;
      i++;
  }
 
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,1 +1,5 @@
-v1.erase(v1.begin()+1);
+while (!q1.empty())
+{
+    cout<<q1.top();
+    q1.pop();
+}
