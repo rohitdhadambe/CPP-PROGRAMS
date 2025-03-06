1035ba8d11a0f839040221f390e904068bc12b90
rohitdhadambe <rohitdhadambe@gmail.com>
2025-03-06 16:28:40 +0530

    no of range

--- NoInRange.cpp
+++ NoInRange.cpp
@@ -1,5 +1,62 @@
-// 
+// #include <iostream>
+// #include <stack>
+// #include <vector>
+// #include <string>
+// #include <cmath>
 
+// using namespace std;
+
+// string convert(int i) {
+//     stack<string> st;
+//     string res;
+
+//     while (i != 0) {
+//         int rem = i % 2;
+//         i = i / 2;
+//         st.push(to_string(rem));
+//     }
+
+//     while (!st.empty()) {
+//         res += st.top();
+//         st.pop();
+//     }
+
+//     return res;
+// }
+
+// int main() {
+//     vector<string> v1;
+//     int L = 10, R = 20, k = 3;
+
+//     for (int i = L; i <= R; i++) {
+//         string S = convert(i);
+//         v1.push_back(S);
+//     }
+
+//     int count = 0;
+//     for (int i = 0; i < v1.size(); i++) {
+//         if (v1[i].find("101") != -1) {
+//             count++;
+//         }
+//     }
+
+//     if (count < k) {
+//         cout << -1;
+//     } else {
+//         int num = stoi(v1[k]), ans = 0, i = 0;
+
+//         while (num != 0) {
+//             int x = num % 10;
+//             num = num / 10;
+//             ans += pow(2, i) * x;
+//             i++;
+//         }
+
+//         cout << ans;
+//     }
+
+//     return 0;
+// }
 
 
 #include <iostream>
@@ -47,9 +104,8 @@ int main() {
     if (count < k) {
         cout << -1;
     } else {
-        int num = stoi(v1[k],0,2);  //decimal conversion
-        cout<<num;
-       
+        int num = stoi(v1[k],0,2);
+          cout<<num;
     }
 
     return 0;
